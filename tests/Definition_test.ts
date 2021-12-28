46eedb52235d02f0b89ff4546cd49a8d297c4ea2
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2021-12-28 18:16:00 +0200

    Add some ByteArrayCompare test cases
    Better than nothing

---
+++ tests/Definition_test.ts
@@ -0,0 +1,35 @@
+import { assert, assertEquals } from "https://deno.land/std@0.119.0/testing/asserts.ts";
+import { AUDALF_Definitions as Definitions } from "../src/AUDALF_Definitions.ts";
+
+Deno.test("ByteArrayCompare equal tests", () => {
+    // Arrange
+    const array1: Uint8Array = new Uint8Array([0, 1, 100, 30, 12, 255]);
+    const array2: Uint8Array = new Uint8Array([0, 1, 100, 30, 12, 255]);
+
+    // Act
+    const shouldBeTrue12 = Definitions.ByteArrayCompare(array1, array2);
+    const shouldBeTrue11 = Definitions.ByteArrayCompare(array1, array1);
+    const shouldBeTrue22 = Definitions.ByteArrayCompare(array2, array2);
+
+    // Assert
+    assertEquals(shouldBeTrue12, true, "Array1 and array2 should be equal");
+    assertEquals(shouldBeTrue11, true, "Array1 and array1 should be equal");
+    assertEquals(shouldBeTrue22, true, "Array2 and array2 should be equal");
+});
+
+Deno.test("ByteArrayCompare not equal tests", () => {
+    // Arrange
+    const array1: Uint8Array = new Uint8Array([0, 1, 100, 30, 12, 255]);
+    const array2: Uint8Array = new Uint8Array([0, 1, 100, 30, 12, 250]);
+    const array3: Uint8Array = new Uint8Array([0, 1, 100, 30, 12]);
+
+    // Act
+    const shouldBeFalse1 = Definitions.ByteArrayCompare(array1, array2);
+    const shouldBeFalse2 = Definitions.ByteArrayCompare(array1, array3);
+    const shouldBeFalse3 = Definitions.ByteArrayCompare(array2, array3);
+
+    // Assert
+    assertEquals(shouldBeFalse1, false, "Array1 and array2 should not be equal");
+    assertEquals(shouldBeFalse2, false, "Array1 and array3 should not be equal");
+    assertEquals(shouldBeFalse3, false, "Array2 and array3 should not be equal");
+});
